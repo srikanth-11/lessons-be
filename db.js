3bf518585e35202057c6aadc0b9f38ef4f8ec68f
Srikanth Kasireddy <kasireddysrikanth27@gmail.com>
2025-06-22 21:20:53 +0530

    Refactor database configuration to use environment-specific settings

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
-const knexConfig = require('./knexfile').development;
+const environment = process.env.NODE_ENV || 'development';
+const knexConfig = require('./knexfile')[environment];
 const knex = require('knex')(knexConfig);
 const { Model } = require('objection');
 
